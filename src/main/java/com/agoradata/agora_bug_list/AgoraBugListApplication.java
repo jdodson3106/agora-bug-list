5bababed539c9c726f7905ff714916567e1eec1c
jdodson3106 <blackflamewebdev@gmail.com>
2020-04-20 20:44:10 -0500

    initial commit

---
+++ src/main/java/com/agoradata/agora_bug_list/AgoraBugListApplication.java
@@ -0,0 +1,13 @@
+package com.agoradata.agora_bug_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgoraBugListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgoraBugListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agoradata/agora_bug_list/AgoraBugListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agoradata.agora_bug_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoraBugListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
